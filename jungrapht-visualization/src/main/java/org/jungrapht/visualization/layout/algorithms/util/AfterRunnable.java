8d09e271acb13aec873d4cce29de96dd3c2de954
tom <thomas.a.nelson@gmail.com>
2019-11-04 06:55:33 -0500

    removed unused code

--- jungrapht-visualization/src/main/java/org/jungrapht/visualization/layout/algorithms/util/AfterRunnable.java
+++ jungrapht-visualization/src/main/java/org/jungrapht/visualization/layout/algorithms/util/AfterRunnable.java
@@ -1,5 +1,8 @@
 package org.jungrapht.visualization.layout.algorithms.util;
 
+/**
+ * Used to cause a LayoutAlgorithm to call vv::scaleToLayout once it has completed
+ */
 public interface AfterRunnable extends Runnable {
 
   void setAfter(Runnable after);
