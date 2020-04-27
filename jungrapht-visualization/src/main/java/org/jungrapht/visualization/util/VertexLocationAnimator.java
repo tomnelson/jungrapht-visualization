43d42672d819b9d7445e5a57a98f7a06540dd6ab
tom <tomnelson@users.noreply.github.com>
2020-04-27 06:43:24 -0400

    update animator

--- jungrapht-visualization/src/main/java/org/jungrapht/visualization/util/VertexLocationAnimator.java
+++ jungrapht-visualization/src/main/java/org/jungrapht/visualization/util/VertexLocationAnimator.java
@@ -1,6 +1,8 @@
 package org.jungrapht.visualization.util;
 
 import java.awt.geom.Point2D;
+import java.util.Collection;
+import java.util.stream.Collectors;
 import org.jungrapht.visualization.MultiLayerTransformer;
 import org.jungrapht.visualization.VisualizationServer;
 import org.jungrapht.visualization.layout.model.LayoutModel;
@@ -8,18 +10,7 @@ import org.jungrapht.visualization.layout.model.Point;
 
 public class VertexLocationAnimator {
 
-  public static <V, E> void scrollVertexToCenter(VisualizationServer<V, E> vv, V vertex) {
-    Point newCenter;
-    if (vertex != null) {
-      // center the selected vertex
-      LayoutModel<V> layoutModel = vv.getVisualizationModel().getLayoutModel();
-      newCenter = layoutModel.apply(vertex);
-    } else {
-      // they did not pick a vertex to center, so
-      // just center the graph
-      Point2D center = vv.getCenter();
-      newCenter = Point.of(center.getX(), center.getY());
-    }
+  public static <V, E> void scrollPointToCenter(VisualizationServer<V, E> vv, Point newCenter) {
     Point2D lvc = vv.getRenderContext().getMultiLayerTransformer().inverseTransform(vv.getCenter());
     final double dx = (lvc.getX() - newCenter.x) / 10;
     final double dy = (lvc.getY() - newCenter.y) / 10;
@@ -39,5 +30,43 @@ public class VertexLocationAnimator {
         };
     Thread thread = new Thread(animator);
     thread.start();
+  }
+
+  public static <V, E> void scrollVertexToCenter(VisualizationServer<V, E> vv, V vertex) {
+    Point newCenter;
+    if (vertex != null) {
+      // center the selected vertex
+      LayoutModel<V> layoutModel = vv.getVisualizationModel().getLayoutModel();
+      newCenter = layoutModel.apply(vertex);
+    } else {
+      // they did not pick a vertex to center, so
+      // just center the graph
+      Point2D center = vv.getCenter();
+      newCenter = Point.of(center.getX(), center.getY());
+    }
+    scrollPointToCenter(vv, newCenter);
+  }
+
+  private static <V, E> Point getPointToCenter(
+      VisualizationServer<V, E> vv, Collection<V> vertices) {
+    LayoutModel<V> layoutModel = vv.getVisualizationModel().getLayoutModel();
+
+    Collection<Point> points = vertices.stream().map(layoutModel).collect(Collectors.toList());
+
+    if (points.size() > 0) {
+      // center the selected vertices
+      return Point.centroidOf(points);
+    }
+
+    // they did not pick a vertex to center, so
+    // just center the graph
+    Point2D center = vv.getCenter();
+    return Point.of(center.getX(), center.getY());
+  }
+
+  public static <V, E> void scrollVertexClusterToCenter(
+      VisualizationServer<V, E> vv, Collection<V> vertices) {
+    Point newCenter = getPointToCenter(vv, vertices);
+    scrollPointToCenter(vv, newCenter);
   }
 }
