e45d20e8e577525cf4e065837daabd1a21e85186
tom <tomnelson@users.noreply.github.com>
2020-06-18 10:59:26 -0400

    update jgrapht version to 1.5.0

--- jungrapht-visualization-samples/src/main/java/org/jungrapht/samples/RectangularLensDemo.java
+++ jungrapht-visualization-samples/src/main/java/org/jungrapht/samples/RectangularLensDemo.java
@@ -46,8 +46,8 @@ import org.jungrapht.visualization.transform.shape.ViewLensSupport;
 
 /**
  * Demonstrates the use of <code>HyperbolicTransform</code> and <code>MagnifyTransform</code>
- * applied to either the model (graph layout) or the view (VisualizationViewer) The hyperbolic
- * transform is applied in an elliptical lens that affects that part of the visualization.
+ * applied to either the model (graph layout) or the view (VisualizationViewer) The transforms are
+ * applied in a rectangular lens that affects that part of the visualization.
  *
  * @author Tom Nelson
  */
