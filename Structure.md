fffdbaf9e1da02dd0979613f5c7ae6ef2c971d78
tom <tomnelson@users.noreply.github.com>
2020-07-19 07:30:51 -0400

    start grooming

---
+++ Structure.md
@@ -0,0 +1,11 @@
+VisualizationViewer adds mouse gestures by extending
+VisualizationServer
+
+VisualizationServer contains code to draw graphs (transforms, renderers)
+
+VisualizationServer contains a VisualizationModel
+
+VisualizationModel contains a LayoutModel
+
+LayoutModel contains a location Map of vertex to Point
+LayoutModel accepts a LayoutAlgorithm to mutate the location Map values
--- jungrapht-visualization/src/main/java/org/jungrapht/visualization/DefaultSatelliteVisualizationViewer.java
+++ jungrapht-visualization/src/main/java/org/jungrapht/visualization/DefaultSatelliteVisualizationViewer.java
@@ -78,7 +78,7 @@ public class DefaultSatelliteVisualizationViewer<V, E> extends DefaultVisualizat
     master
         .getVisualizationModel()
         .getModelChangeSupport()
-        .addModelChangeListener(this::resizeToLayout);
+        .addModelChangeListener(this::scaleToLayout);
 
     // share the selected state of the master
     setSelectedVertexState(master.getSelectedVertexState());
